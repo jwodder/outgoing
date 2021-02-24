8e6f9de5d856069253c465565405a8d3ba3f62a5
John T. Wodder II <git@varonathe.org>
2021-02-24 11:09:02 -0500

    Packaging boilerplate

---
+++ src/outgoing/__init__.py
@@ -0,0 +1,11 @@
+"""
+Abstract interface for different e-mail methods
+
+Visit <https://github.com/jwodder/outgoing> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'outgoing@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/outgoing'
