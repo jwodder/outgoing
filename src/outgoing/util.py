b82ef7b49f5daa24e2746171c259acbffc87a98e
John T. Wodder II <git@varonathe.org>
2021-02-24 23:25:00 -0500

    Rename "configpath" argument to `resolve_path()` to "basepath"

--- src/outgoing/util.py
+++ src/outgoing/util.py
@@ -5,8 +5,8 @@ from typing import Optional, Union
 AnyPath = Union[str, bytes, "os.PathLike[str]", "os.PathLike[bytes]"]
 
 
-def resolve_path(path: AnyPath, configpath: Optional[AnyPath] = None) -> Path:
+def resolve_path(path: AnyPath, basepath: Optional[AnyPath] = None) -> Path:
     p = Path(os.fsdecode(path)).expanduser()
-    if configpath is not None:
-        p = Path(os.fsdecode(configpath)).parent / p
+    if basepath is not None:
+        p = Path(os.fsdecode(basepath)).parent / p
     return p.resolve()
