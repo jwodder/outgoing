a1e65df2cdddd3793834066e31909b6846ee8b4f
John T. Wodder II <git@varonathe.org>
2021-02-26 11:07:16 -0500

    Use an assertion helper for comparing e-mails

--- test/test_main.py
+++ test/test_main.py
@@ -4,8 +4,13 @@ from traceback import format_exception
 from click.testing import CliRunner, Result
 from pytest_mock import MockerFixture
 from outgoing.__main__ import main
+from testing_lib import assert_emails_eq
 
-BODY1 = (
+msg1 = EmailMessage()
+msg1["Subject"] = "Meet me"
+msg1["To"] = "my.beloved@love.love"
+msg1["From"] = "me@here.qq"
+msg1.set_content(
     "Oh my beloved!\n"
     "\n"
     "Wilt thou dine with me on the morrow?\n"
@@ -15,19 +20,11 @@ BODY1 = (
     "Love, Me\n"
 )
 
-msg1 = EmailMessage()
-msg1["Subject"] = "Meet me"
-msg1["To"] = "my.beloved@love.love"
-msg1["From"] = "me@here.qq"
-msg1.set_content(BODY1)
-
-BODY2 = "Hot pockets?  Thou disgusteth me.\n\nPineapple pizza or RIOT.\n"
-
 msg2 = EmailMessage()
 msg2["Subject"] = "No."
 msg2["To"] = "me@here.qq"
 msg2["From"] = "my.beloved@love.love"
-msg2.set_content(BODY2)
+msg2.set_content("Hot pockets?  Thou disgusteth me.\n\nPineapple pizza or RIOT.\n")
 
 
 def show_result(r: Result) -> str:
@@ -53,7 +50,7 @@ def test_main_stdin(mocker: MockerFixture) -> None:
     assert instance.send.call_count == 1
     sent = instance.send.call_args[0][0]
     assert isinstance(sent, EmailMessage)
-    assert sent.get_content() == BODY1
+    assert_emails_eq(sent, msg1)
 
 
 def test_main_args(mocker: MockerFixture) -> None:
@@ -75,7 +72,7 @@ def test_main_args(mocker: MockerFixture) -> None:
     assert instance.send.call_count == 2
     sent1 = instance.send.call_args_list[0][0][0]
     assert isinstance(sent1, EmailMessage)
-    assert sent1.get_content() == BODY1
+    assert_emails_eq(sent1, msg1)
     sent2 = instance.send.call_args_list[1][0][0]
     assert isinstance(sent2, EmailMessage)
-    assert sent2.get_content() == BODY2
+    assert_emails_eq(sent2, msg2)
